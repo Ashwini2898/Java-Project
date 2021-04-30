1cbcbc94e947ad81d9fe0b40f05d3f4be3858029
Ashwini2898 <74813126+Ashwini2898@users.noreply.github.com>
2021-04-30 15:12:38 +0530

    Delete courses directory

--- courses/src/main/java/com/cg/practice/courses/courses/dao/CoursesDao.java
+++
@@ -1,9 +0,0 @@
-package com.cg.practice.courses.courses.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cg.practice.courses.courses.entity.Courses;
-
-public interface CoursesDao extends JpaRepository<Courses,Integer>{
-
-}
